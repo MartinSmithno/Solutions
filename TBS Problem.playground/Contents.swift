5d2c3a9e979501b481504ade3615fe8bf2ec8f85
= <=>
2022-02-08 23:48:38 +0100

    TBS Problem added

---
+++ TBS Problem.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/*
+ * Complete the 'tbsp' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE c
+ *  2. DOUBLE d
+ *  3. 2D_INTEGER_ARRAY cities
+ */
+
+func tbsp(c: Double, d: Double, cities: [[Int]]) -> Void {
+    // Write your code here
+
+}
